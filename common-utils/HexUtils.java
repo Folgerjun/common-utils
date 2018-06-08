ca3651fdff1572c499eacd340afe0da0b50c7e7f
ffj <ffj0721@163.com>
2018-06-08 10:05:59 +0800

    add HexUtil

---
+++ common-utils/HexUtils.java
@@ -0,0 +1,27 @@
+package com.tonglei.test;
+
+public class HexUtils {
+
+	/**
+	 * 字节数组转为十六进制字符串
+	 * 
+	 * @param src
+	 * @return
+	 */
+	public static String bytesToHexString(byte[] src) {
+		StringBuilder stringBuilder = new StringBuilder("");
+		if ((src == null) || (src.length <= 0)) {
+			return null;
+		}
+		for (int i = 0; i < src.length; i++) {
+			int v = src[i] & 0xFF;
+			String hv = Integer.toHexString(v);
+			if (hv.length() < 2) {
+				stringBuilder.append(0);
+			}
+			stringBuilder.append(hv);
+		}
+		return stringBuilder.toString();
+	}
+
+}
